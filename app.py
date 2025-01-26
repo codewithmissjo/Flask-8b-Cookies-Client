abfae74b2216b83556c1a24675f6cc1b7a7d18b5
codewithmissjo <codewithmissjo+jw1@gmail.com>
2025-01-26 11:33:53 -0600

    Updated

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+  value = request.cookies.get('server-sign-in')
+  if value:
+    return render_template("index.html", cookieValue=value)
+  else:
     return render_template("index.html")
+
+@app.route('/sign-in', methods=["POST"])
+def sign_in():
+  value = request.form.get('sign-in')
+  resp = make_response(redirect('/'))
+  resp.set_cookie('server-sign-in', value, max_age = 300)
+  return resp
+
+@app.route('/sign-out')
+def sign_out():
+  resp = make_response(redirect('/'))
+  # the name of the cookie, the value of the cookie, the age of the cookie in seconds
+  resp.set_cookie('server-sign-in', "", max_age = 0)
+  return resp
+
 
 if __name__ == '__main__':
   app.run()
--- static/script.js
+++ static/script.js
@@ -1,22 +1,45 @@
-window.addEventListener('load', checkStatus);
-
-function checkStatus() {
-    if (Cookies.get('clientSignIn')) {
-        $('.client-no').hide()
-        $('.client-yes').show()
-        $('#un').html(Cookies.get('clientSignIn'))
-    } else {
-        $('.client-no').show()
-        $('.client-yes').hide()
-    }
-}
-
 function signIn() {
-    Cookies.set('clientSignIn', $('#sign-in').val(), {path: '/', secure: true, sameSite: 'strict'});
+    // replace this line with code that creates a cookie with the name client-side with the Client's input value
+    Cookies.set('client-side', $('#sign-in').val(), {path: '/', secure: true, sameSite: 'strict'});
     checkStatus();
 }
 
 function signOut() {
-    Cookies.remove('clientSignIn', {path: '/', secure: true, sameSite: 'strict'});
+    // replace this line with code that removes THE client-side cookie
+    Cookies.remove('name');
     checkStatus();
+}
+
+window.addEventListener('load', checkStatus);
+function checkStatus() {
+    // shows and hides html elements based on if the cookie exists or has value
+    var value = Cookies.get('name');
+    if (value) {
+        // the user IS "signed in"
+        // hide the client-no elements using jQuery
+        // show the client-yes elements using jQuery
+        // fill the html of #un with the cookie's value
+    } else {
+        // the user is NOT "signed in"
+        // hide the client-yes elements using jQuery
+        // show the client-no elements using jQuery
+    }
+}
+
+function DontRunJustForExample() {
+
+    // Setting or updating a cookie
+    Cookies.set('name', 'value', {path: '/', secure: true, sameSite: 'strict'});
+
+    // Getting a cookie
+    Cookies.get('name');
+
+    //Getting ALL cookies
+    Cookies.get();
+
+    // Deleting a cookie
+    Cookies.remove('name');
+
+    // getting an input's value
+    $('#sign-in').val();
 }
